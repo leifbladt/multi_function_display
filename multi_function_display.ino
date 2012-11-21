b5105486e0058ceeb8ae50325ce1ff7b16e6788b
Leif Bladt <leif.bladt@gmx.de>
2012-11-21 21:28:04 +0100

    Read temperature from LM35

--- multi_function_display.ino
+++ multi_function_display.ino
@@ -1,5 +1,20 @@
+const int tempPin = A0;
+
 void setup() {
+  delay(500);
+  Serial.begin(9600);
 }
 
 void loop() {
+  Serial.println(getTemp());
+  delay(1000);
 }
+
+float getTemp() {
+  const int temp = analogRead(tempPin);
+  return temp * 0.488;
+}
+
+
+
+
