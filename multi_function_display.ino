0be400925f0e269959dc1574dccfde2c8d1929e1
Leif Bladt <leif.bladt@gmx.de>
2012-11-21 21:40:32 +0100

    Remove whitespace

--- multi_function_display.ino
+++ multi_function_display.ino
@@ -17,18 +17,15 @@ void loop() {
 float getTemp() {
   buffer[bufferPos] = analogRead(tempPin);
   bufferPos = (bufferPos + 1) % BUFFER_SIZE;
-  
+
   int sum = 0;
   for (int i = 0; i < BUFFER_SIZE; i++) {
     sum += buffer[i];
   }
-  
+
   return sum / BUFFER_SIZE * 0.488;
 }
 
 float formatOutput(const float input) {
   return round(input * 10) / float(10);
 }
-
-
-
