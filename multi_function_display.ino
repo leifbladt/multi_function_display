7ca06dd455eb1e7453b2e05e69e7c12a19c147e8
Leif Bladt <leif.bladt@gmx.de>
2012-11-22 19:50:19 +0100

    Extract temperature stuff into class

--- multi_function_display.ino
+++ multi_function_display.ino
@@ -1,8 +1,36 @@
 const int tempPin = A0;
 
-const int BUFFER_SIZE = 3;
-int buffer[BUFFER_SIZE];
-int bufferPos = 0;
+class LM35 {
+  static const int BUFFER_SIZE = 3;
+  int buffer[BUFFER_SIZE];
+  int bufferPos; // FIXME shouldn't be static
+  int _pin;
+
+public:
+  LM35(const int pin) {
+    _pin = pin;
+    bufferPos = 0;
+  }
+
+  float getTemp() {
+    buffer[bufferPos] = analogRead(tempPin);
+    bufferPos = (bufferPos + 1) % BUFFER_SIZE;
+
+    int sum = 0;
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      sum += buffer[i];
+    }
+
+    return formatOutput(sum / BUFFER_SIZE * 0.488);
+  }
+
+private:
+  float formatOutput(const float input) {
+    return round(input * 10) / float(10);
+  }
+};
+
+LM35 lm35(tempPin);
 
 void setup() {
   delay(500);
@@ -10,22 +38,13 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(formatOutput(getTemp()));
+  Serial.println(lm35.getTemp());
   delay(1000);
 }
 
-float getTemp() {
-  buffer[bufferPos] = analogRead(tempPin);
-  bufferPos = (bufferPos + 1) % BUFFER_SIZE;
 
-  int sum = 0;
-  for (int i = 0; i < BUFFER_SIZE; i++) {
-    sum += buffer[i];
-  }
 
-  return sum / BUFFER_SIZE * 0.488;
-}
 
-float formatOutput(const float input) {
-  return round(input * 10) / float(10);
-}
+
+
+
