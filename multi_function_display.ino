36137e01915623658ad91c09ba8edda6e12c55aa
Leif Bladt <leif.bladt@gmx.de>
2012-11-23 21:25:01 +0100

    Introduce pages

--- multi_function_display.ino
+++ multi_function_display.ino
@@ -1,9 +1,12 @@
 #include <LiquidCrystal.h>
 
 const int tempPin = A0;
-const int buttonPin = 7;
+const int buttonPin = 2;
+const int PAGES = 2;
 
 long lastTempTime = 0;
+long lastSwitch = 0;
+int currentPage = 0;
 
 class LM35 {
   static const int BUFFER_SIZE = 3;
@@ -38,72 +41,59 @@ private:
 // TODO Simplify button class
 class Button {
   int _pin;
-  int buttonState;             // the current reading from the input pin
   int lastButtonState;   // the previous reading from the input pin
-
-  // the following variables are long's because the time, measured in miliseconds,
-  // will quickly become a bigger number than can be stored in an int.
-  long lastDebounceTime;  // the last time the output pin was toggled
-  static const long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 public:
   Button(const int pin) {
     _pin = pin;
-    lastDebounceTime = 0;
     lastButtonState = LOW;
   }
 
-  boolean pressed() {
-    // read the state of the switch into a local variable:
-    int reading = digitalRead(buttonPin);
+  boolean released() {
+    const int buttonState = digitalRead(buttonPin);
 
-    // check to see if you just pressed the button 
-    // (i.e. the input went from LOW to HIGH),  and you've waited 
-    // long enough since the last press to ignore any noise:  
-
-    // If the switch changed, due to noise or pressing:
-    if (reading != lastButtonState) {
-      // reset the debouncing timer
-      lastDebounceTime = millis();
-    }    
-
-    if ((millis() - lastDebounceTime) > debounceDelay) {
-      // whatever the reading is at, it's been there for longer
-      // than the debounce delay, so take it as the actual current state:
-      buttonState = reading;
-    }
-    lastButtonState = reading;
+    const boolean released = (buttonState == LOW) && (lastButtonState == HIGH);
+    lastButtonState = buttonState;
+    delay(50); // TODO Debounce without delay
     
-    return buttonState  == HIGH;
+    return released;
   }
 };
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 LM35 lm35(tempPin);
 Button button(buttonPin);
 
 void setup() {
   pinMode(buttonPin, INPUT);
   lcd.begin(16, 2);
-  lcd.print("Temp 1");
   delay(500);
   Serial.begin(9600);
 }
 
 void loop() {
-  if ((millis() - lastTempTime) >= 1000) {
-    lcd.setCursor(11, 0);
-    lcd.print(lm35.getTemp());
-    Serial.println("update temp");
-    lastTempTime = millis();
+   if (button.released()) {
+    currentPage = (currentPage + 1) % PAGES;
+    Serial.println("released");
   }
-
-  if (button.pressed()) {
-    lcd.setCursor(0, 1);
-    lcd.print("Button pressed");
-  } 
-  else {
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
+  
+  if (currentPage == 0) {
+    if ((millis() - lastTempTime) >= 1000) {
+      // TODO Clear only on page switch
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Temp 1");
+      lcd.setCursor(11, 0);
+      lcd.print(lm35.getTemp());
+      lastTempTime = millis();
+    }
+  } else {
+    if ((millis() - lastTempTime) >= 1000) {
+      // TODO Clear only on page switch
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Hello World!");
+      lastTempTime = millis();
+    }
   }
 }
