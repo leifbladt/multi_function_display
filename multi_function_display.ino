d363f802665cccca2937eafa6c8f77451f5b5e1e
Leif Bladt <leif.bladt@gmx.de>
2012-11-23 20:18:49 +0100

    Include push button
    Remove delay from main loop

--- multi_function_display.ino
+++ multi_function_display.ino
@@ -1,7 +1,16 @@
 #include <LiquidCrystal.h>
 
 const int tempPin = A0;
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int buttonPin = 7;
+
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+long lastTempTime = 0;
 
 class LM35 {
   static const int BUFFER_SIZE = 3;
@@ -33,15 +42,51 @@ private:
   }
 };
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 LM35 lm35(tempPin);
 
 void setup() {
+  pinMode(buttonPin, INPUT);
   lcd.begin(16, 2);
-  lcd.print("Temperatur: ");
+  lcd.print("Temp 1");
+  delay(500);
+  Serial.begin(9600);
 }
 
 void loop() {
-  lcd.setCursor(0, 1);
-  lcd.print(lm35.getTemp());
-  delay(1000);
+  if ((millis() - lastTempTime) >= 1000) {
+    lcd.setCursor(11, 0);
+    lcd.print(lm35.getTemp());
+    Serial.println("update temp");
+    lastTempTime = millis();
+  }
+
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button 
+  // (i.e. the input went from LOW to HIGH),  and you've waited 
+  // long enough since the last press to ignore any noise:  
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+    buttonState = reading;
+  }
+
+  if (buttonState == HIGH) {
+    lcd.setCursor(0, 1);
+    lcd.print("Button pressed");
+  } 
+  else {
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+  }
+  lastButtonState = reading;
 }
