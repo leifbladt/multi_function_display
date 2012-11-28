69835406615896647cce9a0229cc4b1e90f5e84e
Leif Bladt <leif.bladt@gmx.de>
2012-11-28 22:59:47 +0100

    First (small) steps on decoupling Measurements and Display

--- multi_function_display.ino
+++ multi_function_display.ino
@@ -14,18 +14,41 @@ long lastBgTime = 0;
 DeviceAddress thermometer = { 
   0x10, 0x5E, 0x98, 0x74, 0x02, 0x08, 0x00, 0xD0 };
 
-class Measurements {
-  
+class Broker {
 public:
-  // TODO Default constructor
-  Measurements(int tmp) : 
+  Broker() : 
+  pos(0) {
+  }
+
+  void subscribe(void (*f)(float)) {
+    _f[pos] = f;
+    pos++;
+  }
+
+  void notify(float value) {
+    for (int i = 0; i < pos; i++) {
+      (_f[i])(value);
+    }
+  }
+
+private:
+  void (*_f[10])(float);
+  int pos;  
+};
+
+class Measurements {
+
+public:
+  Measurements(Broker* b) : 
   _ds(tempPin), _sensors(&_ds), _temp() {
+    _b = b;
     _sensors.begin();
   }
 
   void update() {
     _sensors.requestTemperatures();
     _temp.addValue(_sensors.getTempC(thermometer));
+    _b->notify(getTemp());
   }
 
   float getTemp() {
@@ -36,24 +59,24 @@ private:
   Buffer _temp;
   OneWire _ds;
   DallasTemperature _sensors;
+  Broker* _b;
 };
 
 class Display {
   // TODO Make those private
   static const int PAGES = 2;
   LiquidCrystal _lcd;
-  Measurements& _m;
+  Measurements* _m;
 
   int _currentPage;
 
 public:
-  Display(Measurements& m) :
-  _lcd(12, 11, 5, 4, 3, 2), _m(m)
+  Display(Measurements* m) :
+  _lcd(12, 11, 5, 4, 3, 2)
   {
     _m = m;
     _lcd.begin(16, 2);
     _currentPage = 0;
-
   }
 
   void switchPage() {
@@ -61,7 +84,7 @@ public:
     clear();
     render();
   }
-  
+
   void switchToPage(int page) {
     _currentPage = page;
     clear();
@@ -78,15 +101,16 @@ public:
       _lcd.setCursor(0, 0);
       _lcd.print("Temp:");
       _lcd.setCursor(12, 0);
-      _lcd.print(formatFloat(_m.getTemp()));
-    } else {
+      _lcd.print(formatFloat(_m->getTemp()));
+    } 
+    else {
       _lcd.setCursor(0, 0);
       _lcd.print("Hello World!");
     }
   }
-  
-  private:
-  
+
+private:
+
   char* formatFloat(float input) {
     char c[6];
     dtostrf(round(input * 10) / float(10), 4, 1, c);
@@ -94,16 +118,25 @@ public:
   }
 };
 
-
 Button button(buttonPin);
-Measurements m(0);
-Display display(m);
+Broker b;
+Measurements m(&b);
+Display display(&m);
 
 void setup() {
+  delay(2000);
   pinMode(bgPin, INPUT);
   display.render();
   Serial.begin(9600);
   setBackground();
+
+  b.subscribe(&updateDisplay);
+}
+
+void updateDisplay(float value) {
+  // Update display when the values changed
+  // TODO Use value from broker
+  display.render();
 }
 
 void loop() {
@@ -115,13 +148,12 @@ void loop() {
     display.switchPage();
   }
 
-  // Update screens every 15 seconds
+  // Update measurements every 15 seconds
   if ((millis() - lastMeasureTime) >= 15000) {
     m.update();
-    display.render();
     lastMeasureTime = millis();
   }
-  
+
   if (m.getTemp() > 25.0) {
     display.switchToPage(0);
   }
