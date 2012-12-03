b14b43f78acc02b112568a2356699f9c9aeb5ffa
Leif Bladt <leif.bladt@gmx.de>
2012-12-03 21:58:05 +0100

    Show correct values, although buffer not completely filled

--- Buffer.h
+++ Buffer.h
@@ -13,20 +13,21 @@ private:
   static const int BUFFER_SIZE = 3;
   T buffer[BUFFER_SIZE];
   int bufferPos;
+  int bufferSize;
 };
 
 template <class T>
 void Buffer<T>::addValue(T value) {
   buffer[bufferPos] = value;
   bufferPos = (bufferPos + 1) % BUFFER_SIZE;
+  bufferSize = min(bufferSize + 1, BUFFER_SIZE);
 }
   
 template <class T>
 T Buffer<T>::getValue() {
-  // TODO Handle stored values < BUFFER_SIZE 
   int sum = 0;
   for (int i = 0; i < BUFFER_SIZE; i++) {
     sum += buffer[i];
   }
-  return sum / (T)BUFFER_SIZE;
+  return sum / (T)bufferSize;
 }
